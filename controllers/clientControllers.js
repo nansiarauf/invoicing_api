d009f0abcbf6495e7180620613ca718b9caee4e4
nansia abdul rauf tommie <nansia.rauf@gmail.com>
2021-04-27 03:04:10 -1200

    client controller and validation added

---
+++ controllers/clientControllers.js
@@ -0,0 +1,31 @@
+const Client = required("../models/clientSchema");
+const { clientValidation } = require("../utils/clientValidation");
+
+//ADD NEW CLIENT CONTROLLER
+const addClient = async (req, res) => {
+  //VALIDATE CLIENT INFO
+  const { error } = clientValidation.validate(req.body);
+  if (error) return res.send(error.details[0].message);
+
+  //CHECKING EMAIL AND BUSINESS NAME
+  const emailFound = await Client.findOne({ email: req.body.email });
+  if (emailFound) return res.send("Email already exist");
+
+  const businessName = await Client.findOne({
+    businessName: req.body.businessName,
+  });
+  if (businessName) return res.send("Business already exist");
+
+  //CREATING NEW CLIENT
+  const newClient = new Client({
+    businessName: req.body.businessName,
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    address: req.body.address,
+  });
+  await newClient.save();
+  res.status(201).json(newClient);
+};
+
+module.exports = { addClient };
