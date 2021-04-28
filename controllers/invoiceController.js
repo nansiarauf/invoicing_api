370f1003b2264c8b7684d67c9956409df5a61654
nansia abdul rauf tommie <nansia.rauf@gmail.com>
2021-04-28 04:50:46 -1200

    invoice controllers worked on

--- controllers/invoiceController.js
+++ controllers/invoiceController.js
@@ -21,4 +21,21 @@ const createInvoice = async (req, res) => {
   res.status(201).json(newInvoice);
 };
 
-module.exports = { createInvoice };
+//VIEW ALL SENT OUT INVOICE REMINDERS
+const allInvoices = async (req, res) => {
+  const invoices = await Invoice.find();
+  res.status(201).json(invoices);
+};
+
+//DELETE AN INVOICE REMINDER
+const deleteInvoice = async (req, res) => {
+  const foundInvoice = await Invoice.findById(req.params._id);
+  if (foundInvoice) {
+    foundInvoice.remove();
+    res.json({ msg: `invoice with ID: ${req.params._id} deleted` });
+  } else {
+    res.status(404).json({ msg: `invoice not found ${req.params._id}` });
+  }
+};
+
+module.exports = { createInvoice, allInvoices, deleteInvoice };
